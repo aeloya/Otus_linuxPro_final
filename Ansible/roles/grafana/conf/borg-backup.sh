58d27a6619effcd37daddc134b5601019393f3f3
aeloya <64539462+aeloya@users.noreply.github.com>
2025-05-15 02:29:57 +0300

    Add files via upload

---
+++ Ansible/roles/grafana/conf/borg-backup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK='yes'
+borg create --compression auto,lzma borg@192.168.122.39:grafana_repo::example-`date +%Y%m%d_%H%M%S` /etc /usr /home /var/log
+borg prune --keep-daily 7 --keep-weekly 4 --keep-monthly 1 borg@192.168.122.39:grafana_repo
+
